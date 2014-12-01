df385012cf260862c95a6fa04e21bd1bf60a800b
kichuss <el_kichus_cdl@hotmail.com>
2014-11-30 19:17:06 -0600

    programa finalizado

---
+++ practica5/practica5/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+namespace practica55
+{
+/// <summary>
+/// Description of MySQL.
+/// </summary>
+public class MySQL
+{
+protected MySqlConnection myConnection;
+public MySQL()
+{
+}
+protected void abrirConexion(){
+string connectionString =
+"Server=localhost;" +
+"Database=agenda;" +
+"User ID=root;" +
+"Password=pepe;" +
+"Pooling=false;";
+this.myConnection = new MySqlConnection(connectionString);
+this.myConnection.Open();
+}
+protected void cerrarConexion(){
+this.myConnection.Close();
+this.myConnection = null;
+}
+}
+}
